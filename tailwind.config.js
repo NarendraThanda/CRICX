23ade53b0f581f197396c66d0203daa7ca93b22c
NarendraThanda <nandhuthanda@gmail.com>
2025-04-19 20:40:54 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage :{
+        'match':'url("https://imgs.search.brave.com/My4cfmd1t-06-zqMl4O5tNvWkhsKGAHkRGQ6APqT9Ds/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/MS8xOC8yMDI1X0lQ/TF9sb2dvLnBuZy81/MTJweC0yMDI1X0lQ/TF9sb2dvLnBuZw")',
+        'match1':'url("https://img.jagranjosh.com/images/2024/March/2932024/KKR-vs-RCB-HtH.webp")',
+      }
+
+    },
+  },
+  plugins: [],
+}
